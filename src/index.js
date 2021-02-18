7fa1bdc912f8c1c62bf5e72896d4d93e47fca2e4
DmitryPateew <60105448+DmitryPateew@users.noreply.github.com>
2021-02-18 16:14:15 +0300

    feat: add working alg ro revert int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let mass = n.toString().split('');
+    let revertMass = [];
+    for (let i = mass.length-1; i >= 0; i--) {
+        if (mass[i] === '-') {
+        }else {
+            revertMass.push(mass[i]);
+        }
+    }
+    let res='';
+    for (let el of revertMass) {
+        res += el;
+    }
+    return Number(res);
 }
